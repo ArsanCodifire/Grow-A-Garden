a5500b45e7c10ba8204115aab9c051534e739dcb
Arsan Codifire <arsancodifire@signinid.com>
2025-09-14 02:29:24 +0530

    Create order.py

---
+++ Web/order.py
@@ -0,0 +1,60 @@
+# Seed Order
+SEED_ORDER = [
+    "Carrot",
+    "Strawberry",
+    "Blueberry",
+    "Orange Tulip",
+    "Tomato",
+    "Corn",
+    "Daffodil",
+    "Watermelon",
+    "Pumpkin",
+    "Apple",
+    "Bamboo",
+    "Coconut",
+    "Cactus",
+    "Dragon Fruit",
+    "Mango",
+    "Grape",
+    "Mushroom",
+    "Pepper",
+    "Cacao",
+    "Beanstalk",
+    "Ember Lily",
+    "Sugar Apple",
+    "Burning Bud",
+    "Giant Pinecone",
+    "Elder Strawberry",
+    "Romanesco"
+]
+
+# Egg Order
+EGG_ORDER = [
+    "Basic Egg",
+    "Advanced Egg",
+    "Godly Egg",
+    "Legendary Egg",
+    "Mythical Egg",
+    "Divine Egg",
+    "Prismatic Egg"
+]
+
+# Gear Order
+GEAR_ORDER = [
+    "Watering Can",
+    "Trowel",
+    "Trading Ticket",
+    "Recall Wrench",
+    "Basic Sprinkler",
+    "Advanced Sprinkler",
+    "Medium Treat",
+    "Medium Toy",
+    "Godly Sprinkler",
+    "Master Sprinkler",
+    "Cleaning Spray",
+    "Favorite Tool",
+    "Harvest Tool",
+    "Friendship Pot",
+    "Grandmaster Sprinkler",
+    "Level Up Lollipop"
+]
